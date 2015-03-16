d3ee958a9fb1e32059170464605656c2e9e87fb7
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2015-03-16 09:51:52 +0100

    Move back files to match package with Main

--- src/izhikevich/spikingnetwork/Main.java
+++ src/izhikevich/spikingnetwork/Main.java
@@ -1,14 +1,13 @@
 package izhikevich.spikingnetwork;
 
-import izhikevich.spikingnetwork.runnables.Visualise;
-
 public class Main {
 	/**
 	 * 
 	 */
 	public static void main(String[] args) {
-		//MakeGraph.main("izhikevich.spikingnetwork.MakeGraph");
-		Visualise.main("izhikevich.spikingnetwork.Visualise");
+		MakeGraph.main("izhikevich.spikingnetwork.MakeGraph");
+		//Visualise.main("izhikevich.spikingnetwork.Visualise");
+		//PlotSingleNeuron.main("izhikevich.spikingnetwork.PlotSingleNeuron");
 
 	}
 
