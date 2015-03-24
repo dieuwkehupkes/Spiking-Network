ff8c47e10da9ff77891c85fb28ba4500a42de1e3
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2015-03-24 14:42:24 +0100

    move generic function to separate class with static functions that can be used in other classes

---
+++ src/izhikevich/spikingnetwork/Collection.java
@@ -0,0 +1,36 @@
+package izhikevich.spikingnetwork;
+
+public class Collection {
+	/**
+	 * Collection of functions used throughout the
+	 * package
+	 */
+
+	public static float[][] shift_and_scale(float[][] i, int shiftx, int shifty, double scalex, double scaley) {
+		// function for shifting and scaling values to plot them
+		float[][] output = new float[i.length][i[0].length];
+
+		int counter = 0;
+		for (float[] tuple: i) {
+
+			// apply scaling and shifting of time variable
+			output[counter][0] = tuple[0] + shiftx;
+			output[counter][0] *= scalex;
+
+			// apply scaling and shifting of potential variable
+			output[counter][1] = -1*tuple[1];        // flip sign to get direction right
+			output[counter][1] += shifty;
+			output[counter][1] *= scaley;
+
+			// increase counter
+			counter++;
+		}
+
+		return output;	
+	}
+	
+	/*
+	public static float[][] shift_and_scale()
+	*/
+
+}
