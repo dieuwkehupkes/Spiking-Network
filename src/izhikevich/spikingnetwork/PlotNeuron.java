6c0208830b9c3d12a294263a062307324c1c5bb0
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2015-03-31 15:13:34 +0200

    Investigate influence of time step on neuron behaviour

--- src/izhikevich/spikingnetwork/PlotNeuron.java
+++ src/izhikevich/spikingnetwork/PlotNeuron.java
@@ -12,13 +12,13 @@ public class PlotNeuron extends PApplet {
 	private static final long serialVersionUID = -8259725764770574359L;
 
 	// parameters for neuron to be plotted
-	float I = 10;
+	float I = 100;
 	double a = 0.1;
 	double b = 0.2;
 	double c = -65;
 	double d = 2;
 	
-	int simLength = 200;
+	int simLength = 100;
 
 	// create neuron, set time = 0
 	Neuron n = new Neuron(a, b, c, d);
@@ -40,7 +40,7 @@ public class PlotNeuron extends PApplet {
 
 	
 	public void setup() {
-		size(450, 900);
+		size(450, 400);
 		background(255);
 		fill(0);
 		frameRate(50);
@@ -69,7 +69,7 @@ public class PlotNeuron extends PApplet {
 		
 		if (curIndex == (simLength - 2)) {
 			noLoop();
-			save("../graphs/neuron_statespace_trajectory1.png");
+			// save("../graphs/neuron_statespace_trajectory1.png");
 		}
 		
 		r += colorStep;
@@ -89,13 +89,15 @@ public class PlotNeuron extends PApplet {
 		uvData[curIndex][0] = (float) n.u;
 		uvData[curIndex][1] = (float) n.v;
 
-		float[][] plt = Collection.shift_and_scale(plotData, shiftX, shiftY+250, scaleX, 0.8);
+		float[][] plt = Collection.shift_and_scale(plotData, shiftX, shiftY, scaleX, scaleY);
+		// float[][] plt = Collection.shift_and_scale(plotData, shiftX, shiftY+250, scaleX, 0.8);
 		// scale output and draw plot
 		strokeWeight(1);
 		stroke(0);
 		line(plt[prevIndex][0], plt[prevIndex][1], plt[curIndex][0], plt[curIndex][1]);
 		
-		float[][] plt2 = Collection.shift_and_scale(uvData, shiftX+15, shiftY+2800, scaleX, 0.3);
+		float[][] plt2 = Collection.shift_and_scale(uvData, shiftX+20, shiftY+200, 5, scaleY);
+		// float[][] plt2 = Collection.shift_and_scale(uvData, shiftX+15, shiftY+2800, scaleX, 0.3);
 
 		strokeWeight(2);
 		rgbRed = (int) r;
