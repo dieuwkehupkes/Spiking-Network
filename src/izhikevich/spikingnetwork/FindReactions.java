82e4f5340962d4a9fda668e5bee8bd74a6f6e6c5
Dieuwke Hupkes <dieuwkehupkes@gmail.com>
2015-04-02 09:45:11 +0200

    Add function to plot reaction of output neuron as function of oscillation frequency input neuron

--- src/izhikevich/spikingnetwork/FindReactions.java
+++ src/izhikevich/spikingnetwork/FindReactions.java
@@ -12,25 +12,43 @@ public class FindReactions extends PApplet {
 	 */
 
 	Neuron n1, n2, n3, o1;
+	int[] neighbours;
 	NeuralNetwork network;
 	int Nn = 4;
 	int Nw = 20;
+	float a = (float) 0.02;
+	float b = (float) 0.2;
+	float c = (float) -65;
+	float d = (float) 0.1;
+	double[] weights = {0, 0, 0};
 
 	private static final long serialVersionUID = 7416187811109690854L;
 
 	public void setup() {
 
-		size(1200, 600);
+		// setup window
+		size(900, 500);
 		background(255);
 
+		// draw axis
+		float scaleX = (float) 10;
+		float scaleY = (float) 10;
+
+		int xStart = 30; int xEnd = 400; int yStart = 300; int yEnd = 30;
+		drawAxis(xStart, xEnd, yStart, yEnd);
+		int xBenchValue = 500; int yBenchValue = 500;
+		int xBench = (int) (xStart+xBenchValue/scaleX); int yBench = (int) (yStart - yBenchValue/scaleY);
+		drawScale(xStart, xEnd, xBench, xBenchValue, yStart, yEnd, yBench, yBenchValue);
+		System.out.println("scaledrawn");
+		
 		// set timestep
 		double timeStep = 0.1;
-		int simLength = 3500;
+		int simLength = 1000;
 
 		// create input neurons
-		n1 = new Neuron(0.02, 0.2, -65., 0.1);	// regular spiking neuron
-		n2 = new Neuron(0.02, 0.2, -65., 0.1);	// regular spiking neuron
-		n3 = new Neuron(0.02, 0.2, -65., 0.1);	// regular spiking neuron
+		n1 = new Neuron(a, b, c, d);	// regular spiking neuron
+		n2 = new Neuron(a, b, c, d);	// regular spiking neuron
+		n3 = new Neuron(a, b, c, d);	// regular spiking neuron
 
 		// create output neuron to model
 		o1 = new Neuron(0.02, 0.2, -65, 8);
@@ -40,13 +58,13 @@ public class FindReactions extends PApplet {
 		network = new NeuralNetwork(neurons, Nn, Nw);
 
 		// add neighbours to neuron o1 
-		int[] neighbours = {1, 2, 3};
+		neighbours = new int[]{1, 2, 3};
 		o1.setNeighbours(neighbours);
 
 		// set fixed input for n1, n2 and n3
-		n1.setI(10.0);
-		n2.setI(10.0);
-		n3.setI(10.0);
+		n1.setI(100.0);
+		n2.setI(100.0);
+		n3.setI(100.0);
 
 		// set timestep
 		n1.timeStep = timeStep;
@@ -55,35 +73,73 @@ public class FindReactions extends PApplet {
 		o1.timeStep = timeStep;
 
 		// plot behaviour of input neuron
-
+		
+		/*
 		float[][] plt1 = network.plotV(1, 1000);
 		float[][] pltData = Collection.shift_and_scale(plt1, 40, 400, 2.0, 1.0);
 		for (int i=0; i<plt1.length-1; i++) {
 			line(pltData[i][0], pltData[i][1], pltData[i+1][0], pltData[i+1][1]);
 		}
+		*/
+		
 
-		// plot for different sums of weights
-		double[] weights = {0, 0, 0};
-		float sumWeights =(float) 0.0;
 
+		// Plot nr of spikes against sum inputweights
+		// plotSpikesWeights(simLength, (float) 0.1, (float) 2., xStart, yStart);
+		
+		// plot nr of spikes against input frequence
+		plotSpikesInputFreq(simLength, scaleX, scaleY, xStart, yStart);
+
+		// save("../graphs/reactionsRS3RSi01.jpg");
+	}
+
+	public void draw() {
+
+	}
+	
+	private void plotSpikesInputFreq(int simLength, float scaleX, float scaleY, int x0, int y0) {
+		
+		fill(2);
+		weights[0] = 700; weights[1] = 700; weights[2] = 700;
+		o1.setWeights(weights);
+		
+		for (d= (float) 0.1; d<=20; d+=0.2) {
+			n1.setParameters(a, b, c, d);
+			n2.setParameters(a, b, c, d);
+			n3.setParameters(a, b, c, d);
+			
+			int spikes = network.getNrOfSpikes(3, 20);
+			// int spikes1 = network.getNrOfSpikes(0, 20);
+			// network.plotV(0, simLength);
+			// int spikes2 = n1.getNrOfSpikes(simLength);
+			// int spikes3 = n2.getNrOfSpikes(simLength);
+			
+			//System.out.println("d: "+d+"\tspikes: "+spikes+"\tspikes1: "+spikes1);
+			// System.out.println("d: "+d+"\tspikes: "+spikes);
+			
+			point(scaleX*d+30, -(scaleY*spikes) + 200);
+			
+			}	
+		}
+	
+	@SuppressWarnings("unused")
+	private void plotSpikesWeights(int simLength, float scaleX, float scaleY, int x0, int y0) {
+		// print axis info
 		fill(50);
 		textSize(15);
 		text("sum weights", 390, 235);
 		text("#spikes", 10, 18);
 		textSize(10);
 
-		// draw axis
-		int xStart = 30; int xEnd = 800; int yStart = 200; int yEnd = 30;
-		drawAxis(xStart, xEnd, yStart, yEnd);
-		int x500 = 80; int y50 = 100;
-		drawScale(xStart, x500, xEnd, yStart, y50, yEnd);
-		
-		// generate points and draw plot
+		// plot for different sums of weights
+		float sumWeights =(float) 0.0;
+
+		// set strokeweight
 		strokeWeight(2);
 		for (int i=0; i<simLength; i++) {
 			// increment weights
-			weights[i%3]+= 2;
-			sumWeights+=2;
+			weights[i%3]+= 0.2;
+			sumWeights+=0.2;
 
 			// add weights from neurons n1, n2 and n3 to 01
 			o1.setConnections(neighbours, weights);
@@ -91,41 +147,36 @@ public class FindReactions extends PApplet {
 			network.reset();
 			int nrOfSpikes = network.getNrOfSpikes(3, 100);
 
-			point((float) (0.1*sumWeights+30), -(2*nrOfSpikes) + 200);
-
+			point(scaleX*sumWeights+30, -(scaleY*nrOfSpikes) + 200);
 		}
-
-		// save("../graphs/reactions.jpg");
 	}
 
-	public void draw() {
-
-	}
 
 	private void drawAxis(int xStart, int xEnd, int yStart, int yEnd) {
 		line(xStart, yStart, xStart, yEnd);
 		line(xStart, yStart, xEnd, yStart);
 	}
 
-	private void drawScale(int x0, int x500, int xEnd, int y0, int y50, int yEnd) {
+	private void drawScale(int x0, int xEnd, int xBench, int xBenchValue, int y0, int yEnd, int yBench, int yBenchValue) {
 		// draw x-scale
-		int x = x500;
-		int i = 500;
+		fill(50);
+		int x = xBench;
+		int i = xBenchValue;
 		while (x < xEnd) {
 			line(x, y0-2, x, y0+2);
 			text(i, x-10, y0+15);
-			i += 500;
-			x += (x500-x0);
+			i += xBenchValue;
+			x += (xBench-x0);
 		}
 
 		// draw y-scale
-		int y = y50;
-		i = 50;
+		int y = yBench;
+		i = yBenchValue;
 		while (y > yEnd) {
 			line(x0-2, y, x0+2, y);
 			text(i, x0-25, y+5);
-			i += 50;
-			y += (y50-y0);
+			i += yBenchValue;
+			y += (yBench-y0);
 		}
 	}
 
